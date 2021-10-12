709aa14d80e1dc32e9c2117a5c8d616d5525aecd
Joel Wakefield <joel.wakefield@improving.com>
2021-10-12 00:06:18 -0500

    some formatting changes

--- script.js
+++ script.js
@@ -18,14 +18,35 @@ function focusOnSelection(v, edges = null) {
 }
 
 function addEdgeToFocus(e) {
-  let h = document.createElement('p');
-  h.classList.add('col-1', 'focus-edge-vert');
-  h.textContent = e.head.l + ' -- ' + e.tail.l;
-  focusedElement.edges.appendChild(h);
+  let edge = document.createElement('div');
+  edge.classList.add('focus-edge-container');
+  
+  let h = document.createElement('span');
+  h.classList.add('focus-edge-item','focus-edge-vert');
+  h.textContent = e.head.l;
+  
+  let t = document.createElement('span');
+  t.classList.add('focus-edge-item','focus-edge-vert');
+  t.textContent = e.tail.l;
+
+  let b = document.createElement('button');
+  b.classList.add('focus-edge-item','btn-cancel');
+  b.textContent = 'x';
+
+  edge.appendChild(h);
+  edge.appendChild(t);
+  edge.appendChild(b);
+  focusedElement.edges.appendChild(edge);
 }
 
 focusedElement.title.addEventListener('change', e => {
   if (item !== undefined) {
     item.l = focusedElement.title.value;
+  }
+});
+
+focusedElement.edges.addEventListener('click', (e) => {
+  if (e.target.classList.contains('btn-cancel')) {
+    console.log('cancelled')
   }
 });
